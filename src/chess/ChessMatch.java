8704c89177bc80c5918c826c737dce5c63fc8230
LeoMarinsek <leonardomarinsek@hotmail.com>
2023-01-12 18:31:10 -0300

    Printing possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,6 +25,14 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	// Operação retornando uma matriz de booleanos contendo as posições de movimentos possíveis,
+	// para que a aplicação possa colorir o fundo de cada posição
+	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
+	
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
